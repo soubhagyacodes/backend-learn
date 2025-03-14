58394bb6aa6f0fbd682217018b0a66d2211a896b
soubhagyacodes <soubhagya613mishra@gmail.com>
2025-03-14 23:03:54 +0530

    Backend-Frontend, Proxies

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,34 @@
-require('dotenv').config()
-const express = require('express')
+import 'dotenv/config'
+import e from 'express'
 
-const app = express()
+const app = e()
 const port = process.env.PORT
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
 
-app.get('/json', (req, res) => {
-  res.json()
+app.get('/api/jokes', (req, res) => {
+  const jokes = [
+    {
+      id: 1,
+      joke: 'Why did the scarecrow win an award?',
+      punchline: 'Because he was outstanding in his field.'
+    },
+  
+    {
+      id: 2,
+      joke: 'Why did the tomato turn red?',
+      punchline: 'Because it saw the salad dressing.'
+    },
+  
+    {
+      id: 3,
+      joke: 'What do you call a fake noodle?',
+      punchline: 'An impasta.'
+    }
+  ]
+  res.json(jokes)
 })
 
 app.listen(port, () => {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,39 @@
 import './App.css'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function App() {
   const [jokes, setJokes] = useState([])
 
+  useEffect(() => {
+    axios.get('/api/jokes')
+
+      .then(function (response) {
+        setJokes(response.data)
+      })
+
+      .catch(function (error) {
+        console.log(error);
+      })
+
+  }, [])
+
   return (
     <>
       <h1>JOKES : {jokes.length}</h1>
+
+      <div>
+        {
+          jokes.map((joke) => {
+            return (
+              <div key={joke.id}>
+                <h2>{joke.joke}</h2>
+                <p><i>{joke.punchline}</i></p>
+              </div>
+            )
+          })
+        }
+      </div>
     </>
   )
 }
